f04a61e3afba8eb50d1f52099a49b906aac3a45a
afirth <afirth@users.noreply.github.com>
2021-11-19 06:45:01 +0100

    checkpoint

--- src/app.py
+++ src/app.py
@@ -1,4 +1,5 @@
 import requests
+import re
 import simplejson as json
 import configargparse
 
@@ -15,7 +16,7 @@ headers ={
 }
 headers['Authorization']="Bearer {}".format(options.token)
 
-def run_query(query): # A simple function to use requests.post to make the API call. Note the json= section.
+def run_query(query):
     request = requests.post('https://api.github.com/graphql', json={'query': query}, headers=headers)
     if request.status_code == 200:
         return request.json()
@@ -59,7 +60,44 @@ query = query.replace('MYREPO', options.repo )
 query = query.replace('MYORG', options.owner )
 
 result = run_query(query) # Execute the query
-print(json.dumps(result))
-#print result
-#remaining_rate_limit = result["data"]["rateLimit"]["remaining"] # Drill down the dictionary
-#print("Remaining rate limit - {}".format(remaining_rate_limit))
+print(json.dumps(result, indent=4))
+
+accum = dict()
+# regex for parent detection e.g. "/depends org/repo/1"
+parent_rex = re.compile( '^/depends ([\S]+)$', re.MULTILINE)
+
+
+def add_milestones(accum, milestones):
+    for milestone in milestones:
+        print(milestone['title']) # TODO remove
+        issue_count = milestone['issues']['nodes'].__len__()
+
+        # id: org/repo/milestone_number
+        id = f"{options.owner}/{options.repo}/{milestone['url'].split('/')[-1]}".lower()
+
+        # if description not null, find parents
+        parents = []
+        if milestone['description'] is not None:
+            parents = parent_rex.findall(milestone['description'])
+            for parent in parents:
+                parent = parent.lower()
+                if parent not in accum:
+                    accum[parent] = {'title': "???"}
+
+        # count open issues
+        def condidtion(issue):
+            return issue['state'] == 'OPEN'
+        open_count = sum(map(condidtion, milestone['issues']['nodes']))
+        accum[id] = {
+            'title': milestone['title'],
+            'description': milestone['description'],
+            'url': milestone['url'],
+            'issues_total': issue_count,
+            'issues_open': open_count,
+            'parents': parents,
+        }
+    return accum
+
+accum = add_milestones(accum, result['data']['repository']['milestones']['nodes'])
+print( json.dumps(accum, indent=4))
+
